ddca0e78ee1595c6be23d8771188f3c914c2dfdb
musyoku <pixeltc+github.musyoku@gmail.com>
2018-04-29 22:41:31 +0900

    :thinking:

--- python/neural_mesh_renderer/gui.py
+++ python/neural_mesh_renderer/gui.py
@@ -7,15 +7,25 @@ class Client:
         self.base_url = "http://{}:{}".format(domain, port)
         print(self.base_url)
 
-    def update_object(self, vertices, faces):
+    def pack(self, vertices, faces):
         # 先頭に頂点数を入れ、続けて座標を入れる
-        data = struct.pack("=i{}f".format(vertices.size), vertices.shape[0], *vertices.flatten('F'))
+        return struct.pack("=i{}f".format(vertices.size), vertices.shape[0],
+                           *vertices.flatten("C"))
+
+    def init_object(self, vertices, faces):
+        data = self.pack(vertices, faces)
+        requests.post(
+            url="{}/init_object".format(self.base_url),
+            data=data,
+            headers={'Content-Type': 'application/octet-stream'})
+
+    def update_object(self, vertices, faces):
+        data = self.pack(vertices, faces)
         print(vertices.shape)
-        res = requests.post(
+        requests.post(
             url="{}/update_object".format(self.base_url),
             data=data,
             headers={'Content-Type': 'application/octet-stream'})
-        print(res.status_code)
 
     def on_message(self, ws, message):
         print(message)
--- python/run/train.py
+++ python/run/train.py
@@ -5,10 +5,8 @@ import neural_mesh_renderer as nmr
 
 def main():
     vertices, faces = nmr.load_object("../objects/teapot.obj")
-    for vertex in vertices:
-        print(vertex)
     gui = nmr.gui.Client("localhost", 8080)
-    gui.update_object(vertices, faces)
+    gui.init_object(vertices, faces)
 
 if __name__ == "__main__":
     main()
