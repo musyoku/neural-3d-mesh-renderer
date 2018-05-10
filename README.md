50db51255619056751e2e6077cd684a60214edde
musyoku <pixeltc+github.musyoku@gmail.com>
2018-05-10 16:56:41 +0900

    :thinking:

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 work in progress :construction:
+
+## インストール
+
+**Cython**
+
+```
+pip3 install cython --user
+```
+
+**レンダラーのビルド**
+
+```
+cd python/neural_mesh_renderer/cython/
+python3 setup.py build_ext -i
+```
--- python/neural_mesh_renderer/cython/setup.py
+++ python/neural_mesh_renderer/cython/setup.py
@@ -4,14 +4,13 @@ from Cython.Distutils import build_ext
 from Cython.Build import cythonize
 import numpy
 
-base_dir = "python/neural_mesh_renderer/cython"
 setup(
     ext_modules=cythonize(
         Extension(
             "rasterize_cpu",
             [
-                "{}/rasterize.pyx".format(base_dir),
-                "{}/src/rasterize.c".format(base_dir)
+                "rasterize.pyx",
+                "src/rasterize.c"
             ],
             extra_compile_args=["-O3"],
             language="c",
